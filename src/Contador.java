04430dec2e49b7deab9c3917624ad04534eac2c9
Nelson Oliveira <nelsonjrgg@gmail.com>
2025-07-02 16:57:02 -0300

    Update Contador.java

--- src/Contador.java
+++ src/Contador.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Contador {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o primeiro número:");
+        int numeroUm = sc.nextInt();
+        System.out.println("Digite o segundo número:");
+        int numeroDois = sc.nextInt();
+
+        try {
+            contar(numeroUm, numeroDois);
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro.");
+        }
+
+        sc.close();
+
+    }
+    static void contar(int numeroUm, int numeroDois ) throws ParametrosInvalidosException {
+
+        if (numeroUm > numeroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = numeroDois - numeroUm;
+
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
 }
